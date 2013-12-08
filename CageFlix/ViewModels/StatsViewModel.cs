1d9d89a5012c7ada7f7443f50f53209188b1adbd
markovAntony <abbrown22@gmail.com>
2013-12-07 18:41:28 -0600

    Add ViewModel and View.

---
+++ CageFlix/ViewModels/StatsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CageFlix.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CageFlix.ViewModels
+{
+    public class StatsViewModel : Controller
+    {
+        public Movie LowestRated { get; set; }
+        public Movie HighestRated { get; set; }
+    }
+}
